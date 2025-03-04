ae50e38845755be3b732b2c2dbbf40f8b4b0083a
starble25 <starble25@gmail.com>
2025-03-04 15:15:34 +0900

    프로젝트 초기 세팅

---
+++ src/front/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import MainPage from './pages/main/MainPage';
+
+function App() {
+
+  return (
+    <>
+      <MainPage />
+    </>
+  );
+}
+
+export default App;
---
+++ src/front/src/pages/main/MainPage.js
@@ -0,0 +1,9 @@
+import './MainPage.css';
+
+function MainPage() {
+    return (
+        <h1>mainpage</h1>
+    );
+}
+
+export default MainPage;
