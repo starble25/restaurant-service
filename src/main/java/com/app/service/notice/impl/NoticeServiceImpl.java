794169afecb462ef06d28af756eff2c16cf05890
HEAMIN1011 <mintwo20@nate.com>
2025-03-10 17:13:21 +0900

    공지사항 페이지 어노테이션 추가

--- src/main/java/com/app/service/notice/impl/NoticeServiceImpl.java
+++ src/main/java/com/app/service/notice/impl/NoticeServiceImpl.java
@@ -3,11 +3,13 @@ package com.app.service.notice.impl;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.app.dao.notice.NoticeDAO;
 import com.app.dto.notice.Notice;
 import com.app.service.notice.NoticeService;
 
+@Service
 public class NoticeServiceImpl implements NoticeService {
 
     @Autowired
