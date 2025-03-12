1c73cd3aee10cd241284767acfac5b85593b2167
HEAMIN1011 <mintwo20@nate.com>
2025-03-12 18:39:14 +0900

    게시판 페이지 오류 확인용 코드 삭제

--- src/main/java/com/app/controller/board/BoardController.java
+++ src/main/java/com/app/controller/board/BoardController.java
@@ -3,26 +3,30 @@ package com.app.controller.board;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.app.dto.board.Board;
 import com.app.service.board.BoardService;
 
-@Controller
+@RestController
 public class BoardController {
 	  @Autowired
 	    private BoardService boardService;
 
 	    @GetMapping("/list")
-	    public String boardList(Model model) {
+	    public List<Board> boardList(Model model) {
+	    	System.out.println("/list request");
 	        List<Board> boardList = boardService.getBoardList();
 	        model.addAttribute("boardList", boardList);
-	        return "boardList";
+	        return boardList;
 	    }
 
 	    @GetMapping("/detail/{id}")
@@ -36,13 +40,19 @@ public class BoardController {
 	    public String writeForm() {
 	        return "boardForm";
 	    }
-
+	    
 	    @PostMapping("/write")
-	    public String write(@ModelAttribute Board board) {
-	        boardService.insertBoard(board);
-	        return "redirect:/board/list";
+	    public ResponseEntity<?> write(@RequestBody Board board) {
+	    	System.out.println("/write request");
+	    	
+	        try {
+	            boardService.insertBoard(board);
+	            return ResponseEntity.ok(board); // 저장된 board 객체 반환
+	        } catch (Exception e) {
+	            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("{\"error\": \"게시글 저장 실패\"}");
+	        }
 	    }
-
+	    
 	    @GetMapping("/edit/{id}")
 	    public String editForm(@PathVariable int id, Model model) {
 	        Board board = boardService.getBoardById(id);
